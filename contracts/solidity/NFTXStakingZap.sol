12d716685639026a53c8bf6847964a79d208a0d1
Ivan Martinez <ivanthegreatdev@gmail.com>
2021-07-12 16:40:26 -0400

    cleanup

--- contracts/solidity/NFTXStakingZap.sol
+++ contracts/solidity/NFTXStakingZap.sol
@@ -12,7 +12,6 @@ import "./token/IERC20Upgradeable.sol";
 import "./token/ERC721HolderUpgradeable.sol";
 import "./token/ERC1155HolderUpgradeable.sol";
 import "./util/OwnableUpgradeable.sol";
-import "./util/ReentrancyGuardUpgradeable.sol";
 
 // Authors: @0xKiwi_.
 
@@ -22,10 +21,134 @@ interface IWETH {
   function withdraw(uint) external;
 }
 
-contract NFTXStakingZap is OwnableUpgradeable, ReentrancyGuardUpgradeable, ERC721HolderUpgradeable, ERC1155HolderUpgradeable {
+/**
+ * @dev Contract module that helps prevent reentrant calls to a function.
+ *
+ * Inheriting from `ReentrancyGuard` will make the {nonReentrant} modifier
+ * available, which can be applied to functions to make sure there are no nested
+ * (reentrant) calls to them.
+ *
+ * Note that because there is a single `nonReentrant` guard, functions marked as
+ * `nonReentrant` may not call one another. This can be worked around by making
+ * those functions `private`, and then adding `external` `nonReentrant` entry
+ * points to them.
+ *
+ * TIP: If you would like to learn more about reentrancy and alternative ways
+ * to protect against it, check out our blog post
+ * https://blog.openzeppelin.com/reentrancy-after-istanbul/[Reentrancy After Istanbul].
+ */
+abstract contract ReentrancyGuard {
+    // Booleans are more expensive than uint256 or any type that takes up a full
+    // word because each write operation emits an extra SLOAD to first read the
+    // slot's contents, replace the bits taken up by the boolean, and then write
+    // back. This is the compiler's defense against contract upgrades and
+    // pointer aliasing, and it cannot be disabled.
+
+    // The values being non-zero value makes deployment a bit more expensive,
+    // but in exchange the refund on every call to nonReentrant will be lower in
+    // amount. Since refunds are capped to a percentage of the total
+    // transaction's gas, it is best to keep them low in cases like this one, to
+    // increase the likelihood of the full refund coming into effect.
+    uint256 private constant _NOT_ENTERED = 1;
+    uint256 private constant _ENTERED = 2;
+
+    uint256 private _status;
+
+    constructor() {
+        _status = _NOT_ENTERED;
+    }
+
+    /**
+     * @dev Prevents a contract from calling itself, directly or indirectly.
+     * Calling a `nonReentrant` function from another `nonReentrant`
+     * function is not supported. It is possible to prevent this from happening
+     * by making the `nonReentrant` function external, and make it call a
+     * `private` function that does the actual work.
+     */
+    modifier nonReentrant() {
+        // On the first call to nonReentrant, _notEntered will be true
+        require(_status != _ENTERED, "ReentrancyGuard: reentrant call");
+
+        // Any calls to nonReentrant after this point will fail
+        _status = _ENTERED;
+
+        _;
+
+        // By storing the original value once again, a refund is triggered (see
+        // https://eips.ethereum.org/EIPS/eip-2200)
+        _status = _NOT_ENTERED;
+    }
+}
+/**
+ * @dev Contract module which provides a basic access control mechanism, where
+ * there is an account (an owner) that can be granted exclusive access to
+ * specific functions.
+ *
+ * By default, the owner account will be the one that deploys the contract. This
+ * can later be changed with {transferOwnership}.
+ *
+ * This module is used through inheritance. It will make available the modifier
+ * `onlyOwner`, which can be applied to your functions to restrict their use to
+ * the owner.
+ */
+abstract contract Ownable {
+    address private _owner;
+
+    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
+
+    /**
+     * @dev Initializes the contract setting the deployer as the initial owner.
+     */
+    constructor() {
+        _setOwner(msg.sender);
+    }
+
+    /**
+     * @dev Returns the address of the current owner.
+     */
+    function owner() public view virtual returns (address) {
+        return _owner;
+    }
+
+    /**
+     * @dev Throws if called by any account other than the owner.
+     */
+    modifier onlyOwner() {
+        require(owner() == msg.sender, "Ownable: caller is not the owner");
+        _;
+    }
+
+    /**
+     * @dev Leaves the contract without owner. It will not be possible to call
+     * `onlyOwner` functions anymore. Can only be called by the current owner.
+     *
+     * NOTE: Renouncing ownership will leave the contract without an owner,
+     * thereby removing any functionality that is only available to the owner.
+     */
+    function renounceOwnership() public virtual onlyOwner {
+        _setOwner(address(0));
+    }
+
+    /**
+     * @dev Transfers ownership of the contract to a new account (`newOwner`).
+     * Can only be called by the current owner.
+     */
+    function transferOwnership(address newOwner) public virtual onlyOwner {
+        require(newOwner != address(0), "Ownable: new owner is the zero address");
+        _setOwner(newOwner);
+    }
+
+    function _setOwner(address newOwner) private {
+        address oldOwner = _owner;
+        _owner = newOwner;
+        emit OwnershipTransferred(oldOwner, newOwner);
+    }
+}
+
+contract NFTXStakingZap is Ownable, ReentrancyGuard, ERC721HolderUpgradeable, ERC1155HolderUpgradeable {
   IWETH public WETH; 
   INFTXLPStaking public lpStaking;
-  INFTXVaultFactory public nftxFactory;
+  INFTXVaultFactory public immutable nftxFactory;
   IUniswapV2Router01 public sushiRouter;
 
   uint256 public lockTime = 48 hours; 
@@ -38,16 +161,19 @@ contract NFTXStakingZap is OwnableUpgradeable, ReentrancyGuardUpgradeable, ERC72
   event UserStaked(uint256 vaultId, uint256 count, uint256 lpBalance, uint256 timelockUntil);
   event Withdraw(uint256 vaultId, uint256 lpBalance);
 
-  constructor(address _nftxFactory, address _sushiRouter) {
-    __Ownable_init();
+  constructor(address _nftxFactory, address _sushiRouter) Ownable() ReentrancyGuard() {
     nftxFactory = INFTXVaultFactory(_nftxFactory);
     sushiRouter = IUniswapV2Router01(_sushiRouter);
     WETH = IWETH(sushiRouter.WETH());
     IERC20Upgradeable(address(WETH)).approve(address(sushiRouter), type(uint256).max);
   }
 
-  function setLpStakingAddress(address newLpStaking) external {
+  function setLpStakingAddress(address newLpStaking) external onlyOwner {
     lpStaking = INFTXLPStaking(newLpStaking);
+  } 
+
+  function setLockTime(uint256 newLockTime) external onlyOwner {
+    lockTime = newLockTime;
   } 
 
   function addLiquidity721ETH(
@@ -134,10 +260,6 @@ contract NFTXStakingZap is OwnableUpgradeable, ReentrancyGuardUpgradeable, ERC72
 
     emit Withdraw(vaultId, lockedBal);
   }
-
-  function setLockTime(uint256 newLockTime) external onlyOwner {
-    lockTime = newLockTime;
-  } 
 
   function lockedUntil(uint256 vaultId, address who) external view returns (uint256) {
     return zapLock[vaultId][who];
