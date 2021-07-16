50caf37e8cb781797458180add5a144de4b985de
Ivan Martinez <ivanthegreatdev@gmail.com>
2021-07-16 16:11:44 -0400

    Clenan zaps

--- contracts/solidity/NFTXStakingZap.sol
+++ contracts/solidity/NFTXStakingZap.sol
@@ -79,6 +79,7 @@ abstract contract ReentrancyGuard {
         _status = _NOT_ENTERED;
     }
 }
+
 /**
  * @dev Contract module which provides a basic access control mechanism, where
  * there is an account (an owner) that can be granted exclusive access to
