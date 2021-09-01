d37fc2b82638747bffece02027d2f3e7fa925086
Ivan Martinez <ivanthegreatdev@gmail.com>
2021-09-01 01:06:00 -0400

    Unstuck fee script

---
+++ scripts/claim-stuck-fees.js
@@ -0,0 +1,35 @@
+const { BigNumber } = require("@ethersproject/bignumber");
+const { ethers, upgrades } = require("hardhat");
+
+const notZeroAddr = "0x000000000000000000000000000000000000dead";
+
+async function main() {
+  const [deployer] = await ethers.getSigners();
+
+  console.log("Deploying account:", await deployer.getAddress());
+  console.log(
+    "Deploying account balance:",
+    (await deployer.getBalance()).toString(),
+    "\n"
+  );
+  
+  const factory = await ethers.getContractAt("NFTXVaultFactoryUpgradeable", "0xBE86f647b167567525cCAAfcd6f881F1Ee558216")
+  const numVaults = await factory.numVault(); 
+
+  for (let i = ethers.BigNumber.from(0); i.lt(numVaults); i.add(1)) {
+    const vault = await ethers.getContractAt("NFTXVaultUpgradeable", await factory.vault(i))
+    await vault.saveStuckFees();
+  }
+  
+}
+
+main()
+  .then(() => {
+    console.log("\nDeployment completed successfully ✓");
+    process.exit(0);
+  })
+  .catch((error) => {
+    console.log("\nDeployment failed ✗");
+    console.error(error);
+    process.exit(1);
+  });
