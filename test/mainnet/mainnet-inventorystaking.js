b749ddf34e8e75d6c9576b4a064ce83237e95a39
Ivan Martinez <ivanthegreatdev@gmail.com>
2021-12-02 13:18:15 -0500

    remove

--- test/mainnet/mainnet-inventorystaking.js
+++ test/mainnet/mainnet-inventorystaking.js
@@ -128,7 +128,6 @@ describe("Inventory Staking Test", function () {
       "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F" /* Sushi Router */
     );
     await zap.deployed();
-    await nftx.connect(dao).setZapContract(zap.address);
     await nftx.connect(dao).setFeeExclusion(zap.address, true);
   })
 
