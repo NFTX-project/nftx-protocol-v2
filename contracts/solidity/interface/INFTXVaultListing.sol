36f3930c0589d7937e13ead347c246926bd1cbb4
Tom Wade <twade7@gmail.com>
2022-03-30 17:06:30 +0100

    Update interface

--- contracts/solidity/interface/INFTXVaultListing.sol
+++ contracts/solidity/interface/INFTXVaultListing.sol
@@ -5,22 +5,26 @@ pragma solidity ^0.8.0;
 
 interface INFTXVaultListing {
 
-	function createListings(
-		uint256[] calldata nftIds,
-		address[] calldata vaults,
-		uint256[] calldata prices,
-		uint256[] calldata expiries
-	) external;
+    function createListings(
+        uint256[] calldata nftIds,
+        address[] calldata vaults,
+        uint256[] calldata prices,
+        uint256[] calldata expiries
+    ) external;
 
-	function updateListings(
-		uint256[] calldata nftIds,
-		address[] calldata vaults,
-		uint256[] calldata expiries
-	) external;
+    function updateListings(
+        uint256[] calldata nftIds,
+        address[] calldata vaults,
+        uint256[] calldata expiries
+    ) external;
 
-	function fillListings(
-		uint256[] calldata nftIds,
-		address[] calldata vaults
-	) external;
+    function fillListings(
+        uint256[] calldata nftIds,
+        address[] calldata vaults
+    ) external;
+
+    function getListings(
+        address[] calldata vaults
+    ) external;
 
 }
