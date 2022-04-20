7c9d9877fb08e2626df6a474d803bc4aa05f033c
Ivan Martinez <ivanthegreatdev@gmail.com>
2022-04-20 02:51:45 -0400

    Add erc21upgradeable

--- contracts/solidity/NFTXUniV3Staking.sol
+++ contracts/solidity/NFTXUniV3Staking.sol
@@ -10,10 +10,7 @@ import "./token/IERC20Metadata.sol";
 import "./util/SafeERC20Upgradeable.sol";
 import "./util/PausableUpgradeable.sol";
 import "./util/Address.sol";
-import "./util/Create2.sol";
-import "./proxy/UpgradeableBeacon.sol";
-import "./proxy/Create2BeaconProxy.sol";
-import "./token/XTokenUpgradeable.sol";
+import "./token/ERC721Upgradeable.sol";
 import "./univ3/INonfungiblePositionManager.sol";
 import "./univ3/PoolAddress.sol";
 
@@ -22,13 +19,12 @@ import "./univ3/PoolAddress.sol";
 // Pausing codes for inventory staking are:
 // 10: Deposit
 
-contract NFTXInventoryStaking is PausableUpgradeable, UpgradeableBeacon {
+contract NFTXInventoryStaking is PausableUpgradeable, ERC721Upgradeable {
     using SafeERC20Upgradeable for IERC20Upgradeable;
 
     // Small locktime to prevent flash deposits.
     uint256 internal constant DEFAULT_LOCKTIME = 2;
     address public defaultPair;
-    bytes internal constant beaconCode = type(Create2BeaconProxy).creationCode;
         /// @dev The minimum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**-128
     int24 internal constant MIN_TICK = -887272;
     /// @dev The maximum tick that may be passed to #getSqrtRatioAtTick computed from log base 1.0001 of 2**128
@@ -52,8 +48,6 @@ contract NFTXInventoryStaking is PausableUpgradeable, UpgradeableBeacon {
         v3Factory = _v3Factory;
         nftManager = INonfungiblePositionManager(_nftManager);
         defaultPair = _defaultPair;
-        address xTokenImpl = address(new XTokenUpgradeable());
-        __UpgradeableBeacon__init(xTokenImpl);
     }
 
     modifier onlyAdmin() {
@@ -88,9 +82,14 @@ contract NFTXInventoryStaking is PausableUpgradeable, UpgradeableBeacon {
     function addLiquidityToVaultV3Position(uint256 vaultId, uint256 amount0, uint256 amount1) public {
       uint256 tokenId = vaultV3PositionId[vaultId];
       require(tokenId != 0, "No Vault V3 position");
+      address _vaultToken = nftxVaultFactory.vault(vaultId);
+      address _defaultPair = defaultPair;
+      (address address0, address address1) = sortTokens(_vaultToken, _defaultPair);
 
-      // pull tokens 
-      // approve position manager
+      IERC20Upgradeable(address0).transferFrom(msg.sender, address(this), amount0);
+      IERC20Upgradeable(address1).transferFrom(msg.sender, address(this), amount1);
+      IERC20Upgradeable(address0).approve(address(nftManager), amount0);
+      IERC20Upgradeable(address1).approve(address(nftManager), amount1);
 
       (,,,,,,, uint128 oldLiquidity,,,,) = nftManager.positions(tokenId);
 
@@ -104,95 +103,59 @@ contract NFTXInventoryStaking is PausableUpgradeable, UpgradeableBeacon {
       });
       (uint128 newLiquidity, uint256 amount0, uint256 amount1) = nftManager.increaseLiquidity(params);
 
-      // _mintTokensForLiquidityAdded(vaultId, oldLiquidity, newLiquidity);
+      // _mintNFT(vaultId, oldLiquidity, newLiquidity);
     }
 
-    function deployXTokenForVault(uint256 vaultId) public virtual {
-      address vaultToken = nftxVaultFactory.vault(vaultId);
-      address deployedXToken = xTokenAddr(address(vaultToken));
-
-      if (isContract(deployedXToken)) {
-          return;
-      }
-
-      address xToken = _deployXToken(vaultToken);
-      emit XTokenCreated(vaultId, vaultToken, xToken);
-    }
-
-    function receiveRewards(uint256 vaultId, uint256 amount) external virtual onlyAdmin returns (bool) {
-      address vaultToken = nftxVaultFactory.vault(vaultId);
-      address deployedXToken = xTokenAddr(address(vaultToken));
+    // function receiveRewards(uint256 vaultId, uint256 amount) external virtual onlyAdmin returns (bool) {
+    //   address vaultToken = nftxVaultFactory.vault(vaultId);
+    //   address deployedXToken = xTokenAddr(address(vaultToken));
       
-      // Don't distribute rewards unless there are people to distribute to.
-      // Also added here if the distribution token is not deployed, just forfeit rewards for now.
-      if (!isContract(deployedXToken) || XTokenUpgradeable(deployedXToken).totalSupply() == 0) {
-          return false;
-      }
-      // We "pull" to the dividend tokens so the fee distributor only needs to approve this contract.
-      IERC20Upgradeable(vaultToken).safeTransferFrom(msg.sender, deployedXToken, amount);
-      emit FeesReceived(vaultId, amount);
-      return true;
-    }
 
-   function xTokenShareValue(uint256 vaultId) external view virtual returns (uint256) {
-      uint256 tokenId = vaultV3PositionId[vaultId];
-      require(tokenId != 0, "No Vault V3 position");
+    //   // We "pull" to the dividend tokens so the fee distributor only needs to approve this contract.
+    //   IERC20Upgradeable(vaultToken).safeTransferFrom(msg.sender, deployedXToken, amount);
+    //   emit FeesReceived(vaultId, amount);
+    //   return true;
+    // }
 
-      (,,,,,,, uint128 liquidity,,,,) = nftManager.positions(tokenId);
+//    function xTokenShareValue(uint256 vaultId) external view virtual returns (uint256) {
+//       uint256 tokenId = vaultV3PositionId[vaultId];
+//       require(tokenId != 0, "No Vault V3 position");
 
-      address vaultToken = nftxVaultFactory.vault(vaultId);
-      XTokenUpgradeable xToken = XTokenUpgradeable(xTokenAddr(address(vaultToken)));
-      require(address(xToken) != address(0), "XToken not deployed");
+//       (,,,,,,, uint128 liquidity,,,,) = nftManager.positions(tokenId);
 
-      uint256 multiplier = 10 ** 18;
-      return xToken.totalSupply() > 0 
-          ? multiplier * liquidity / xToken.totalSupply() 
-          : multiplier;
-    }
+//       address vaultToken = nftxVaultFactory.vault(vaultId);
+//       XTokenUpgradeable xToken = XTokenUpgradeable(xTokenAddr(address(vaultToken)));
+//       require(address(xToken) != address(0), "XToken not deployed");
 
-    function timelockUntil(uint256 vaultId, address who) external view returns (uint256) {
-        XTokenUpgradeable xToken = XTokenUpgradeable(vaultXToken(vaultId));
-        return xToken.timelockUntil(who);
-    }
+//       uint256 multiplier = 10 ** 18;
+//       return xToken.totalSupply() > 0 
+//           ? multiplier * liquidity / xToken.totalSupply() 
+//           : multiplier;
+//     }
 
-    function balanceOf(uint256 vaultId, address who) external view returns (uint256) {
-        XTokenUpgradeable xToken = XTokenUpgradeable(vaultXToken(vaultId));
-        return xToken.balanceOf(who);
-    }
+    // function timelockUntil(uint256 vaultId, address who) external view returns (uint256) {
+    //     XTokenUpgradeable xToken = XTokenUpgradeable(vaultXToken(vaultId));
+    //     return xToken.timelockUntil(who);
+    // }
+
+    // function balanceOf(uint256 vaultId, address who) external view returns (uint256) {
+    //     XTokenUpgradeable xToken = XTokenUpgradeable(vaultXToken(vaultId));
+    //     return xToken.balanceOf(who);
+    // }
 
     // Note: this function does not guarantee the token is deployed, we leave that check to elsewhere to save gas.
-    function xTokenAddr(address baseToken) public view virtual returns (address) {
-        bytes32 salt = keccak256(abi.encodePacked(baseToken, defaultPair));
-        address tokenAddr = Create2.computeAddress(salt, keccak256(type(Create2BeaconProxy).creationCode));
-        return tokenAddr;
-    }
+    // function xTokenAddr(address baseToken) public view virtual returns (address) {
+    //     bytes32 salt = keccak256(abi.encodePacked(baseToken, defaultPair));
+    //     address tokenAddr = Create2.computeAddress(salt, keccak256(type(Create2BeaconProxy).creationCode));
+    //     return tokenAddr;
+    // }
     
-    function vaultXToken(uint256 vaultId) public view virtual returns (address) {
-        address baseToken = nftxVaultFactory.vault(vaultId);
-        address xToken = xTokenAddr(baseToken);
-        require(isContract(xToken), "XToken not deployed");
-        return xToken;
-    } 
-
-    function _timelockMintFor(uint256 vaultId, address account, uint256 _amount, uint256 timelockLength) internal returns (IERC20Upgradeable, XTokenUpgradeable, uint256) {
-        deployXTokenForVault(vaultId);
-        IERC20Upgradeable baseToken = IERC20Upgradeable(nftxVaultFactory.vault(vaultId));
-        XTokenUpgradeable xToken = XTokenUpgradeable((xTokenAddr(address(baseToken))));
-
-        uint256 xTokensMinted = xToken.mintXTokens(account, _amount, timelockLength);
-        return (baseToken, xToken, xTokensMinted);
-    }
-
-    // Assumes its paired with the default pair.
-    function _deployXToken(address vaultToken) internal returns (address) {
-        string memory symbol = IERC20Metadata(vaultToken).symbol();
-        string memory pairSymbol = IERC20Metadata(defaultPair).symbol();
-        symbol = string(abi.encodePacked("x", symbol, pairSymbol));
-        bytes32 salt = keccak256(abi.encodePacked(vaultToken, defaultPair));
-        address deployedXToken = Create2.deploy(0, salt, beaconCode);
-        XTokenUpgradeable(deployedXToken).__XToken_init(vaultToken, symbol, symbol);
-        return deployedXToken;
-    }
+    // function vaultXToken(uint256 vaultId) public view virtual returns (address) {
+    //     address baseToken = nftxVaultFactory.vault(vaultId);
+    //     address xToken = xTokenAddr(baseToken);
+    //     require(isContract(xToken), "XToken not deployed");
+    //     return xToken;
+    // } 
 
     function isContract(address account) internal view returns (bool) {
         // This method relies on extcodesize, which returns 0 for contracts in
@@ -203,5 +166,12 @@ contract NFTXInventoryStaking is PausableUpgradeable, UpgradeableBeacon {
         // solhint-disable-next-line no-inline-assembly
         assembly { size := extcodesize(account) }
         return size != 0;
+    }
+
+     // returns sorted token addresses, used to handle return values from pairs sorted in this order
+    function sortTokens(address tokenA, address tokenB) internal pure returns (address token0, address token1) {
+        require(tokenA != tokenB, 'UniswapV2Library: IDENTICAL_ADDRESSES');
+        (token0, token1) = tokenA < tokenB ? (tokenA, tokenB) : (tokenB, tokenA);
+        require(token0 != address(0), 'UniswapV2Library: ZERO_ADDRESS');
     }
 }
