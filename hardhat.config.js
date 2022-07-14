660d2cbafb084b0ff8997d89c0111e4f6b653ed4
Tom Wade <twade7@gmail.com>
2022-07-14 20:39:35 +0100

    Add deploy script
    # Conflicts:
    #	hardhat.config.js

--- hardhat.config.js
+++ hardhat.config.js
@@ -3,7 +3,6 @@ require("@nomiclabs/hardhat-waffle");
 require("@openzeppelin/hardhat-upgrades");
 require("hardhat-gas-reporter");
 require("@nomiclabs/hardhat-etherscan");
-require('hardhat-docgen');
 
 /**
  * @type import('hardhat/config').HardhatUserConfig
@@ -14,21 +13,14 @@ module.exports = {
       url: `https://eth-rinkeby.alchemyapi.io/v2/${process.env.ALCHEMY_RINKEBY_API_KEY}`,
       accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
     },
-    ropsten: {
-      url: `https://eth-ropsten.alchemyapi.io/v2/${process.env.ALCHEMY_ROPSTEN_API_KEY}`,
-      accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
-      timeout: 600000,
-    },
     goerli: {
-      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_GOERLI_API_KEY}`,
+      url: `https://eth-goerli.alchemyapi.io/v2/${process.env.ALCHEMY_RINKEBY_API_KEY}`,
       accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
-      gasPrice: 2500000000,
     },
     mainnet: {
       url: `https://eth-mainnet.alchemyapi.io/v2/${process.env.ALCHEMY_MAINNET_API_KEY}`,
       accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
-      timeout: 60000,
-      // gasPrice: 75000000000,
+      gasPrice: 200000000000,
     },
     palm: {
       url: `https://palm-mainnet.infura.io/v3/${process.env.PALM_API_KEY}`,
@@ -37,10 +29,7 @@ module.exports = {
     arbitrum: {
       url: "https://arb1.arbitrum.io/rpc",
       accounts: [`0x${process.env.DEV_PRIVATE_KEY}`],
-      timeout: 600000,
-    },
-    frame: {
-      url: "http://127.0.0.1:1248",
+      timeout: 100000,
     },
     hardhat: {
       mining: {
