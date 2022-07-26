4d6369ee926c2218ff4b3d1665bc230d9eaccfbb
Tom Wade <twade7@gmail.com>
2022-07-26 09:54:33 +0100

    Streamline the code

--- contracts/solidity/eligibility/NFTXMerkleEligibility.sol
+++ contracts/solidity/eligibility/NFTXMerkleEligibility.sol
@@ -4,13 +4,8 @@ pragma solidity ^0.8.0;
 
 import "./NFTXEligibility.sol";
 
-import "hardhat/console.sol";
-
 
 library Strings {
-    bytes16 private constant _HEX_SYMBOLS = "0123456789abcdef";
-    uint8 private constant _ADDRESS_LENGTH = 20;
-
     /**
      * @dev Converts a `uint256` to its ASCII `string` decimal representation.
      */
@@ -34,44 +29,6 @@ library Strings {
             value /= 10;
         }
         return string(buffer);
-    }
-
-    /**
-     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation.
-     */
-    function toHexString(uint256 value) internal pure returns (string memory) {
-        if (value == 0) {
-            return "0x00";
-        }
-        uint256 temp = value;
-        uint256 length = 0;
-        while (temp != 0) {
-            length++;
-            temp >>= 8;
-        }
-        return toHexString(value, length);
-    }
-
-    /**
-     * @dev Converts a `uint256` to its ASCII `string` hexadecimal representation with fixed length.
-     */
-    function toHexString(uint256 value, uint256 length) internal pure returns (string memory) {
-        bytes memory buffer = new bytes(2 * length + 2);
-        buffer[0] = "0";
-        buffer[1] = "x";
-        for (uint256 i = 2 * length + 1; i > 1; --i) {
-            buffer[i] = _HEX_SYMBOLS[value & 0xf];
-            value >>= 4;
-        }
-        require(value == 0, "Strings: hex length insufficient");
-        return string(buffer);
-    }
-
-    /**
-     * @dev Converts an `address` with fixed length of 20 bytes to its not checksummed ASCII `string` hexadecimal representation.
-     */
-    function toHexString(address addr) internal pure returns (string memory) {
-        return toHexString(uint256(uint160(addr)), _ADDRESS_LENGTH);
     }
 }
 
@@ -107,19 +64,6 @@ library MerkleProof {
     }
 
     /**
-     * @dev Calldata version of {verify}
-     *
-     * _Available since v4.7._
-     */
-    function verifyCalldata(
-        bytes32[] calldata proof,
-        bytes32 root,
-        bytes32 leaf
-    ) internal pure returns (bool) {
-        return processProofCalldata(proof, leaf) == root;
-    }
-
-    /**
      * @dev Returns the rebuilt hash obtained by traversing a Merkle tree up
      * from `leaf` using `proof`. A `proof` is valid if and only if the rebuilt
      * hash matches the root of the tree. When processing the proof, the pairs
@@ -133,142 +77,6 @@ library MerkleProof {
             computedHash = _hashPair(computedHash, proof[i]);
         }
         return computedHash;
-    }
-
-    /**
-     * @dev Calldata version of {processProof}
-     *
-     * _Available since v4.7._
-     */
-    function processProofCalldata(bytes32[] calldata proof, bytes32 leaf) internal pure returns (bytes32) {
-        bytes32 computedHash = leaf;
-        for (uint256 i = 0; i < proof.length; i++) {
-            computedHash = _hashPair(computedHash, proof[i]);
-        }
-        return computedHash;
-    }
-
-    /**
-     * @dev Returns true if the `leaves` can be proved to be a part of a Merkle tree defined by
-     * `root`, according to `proof` and `proofFlags` as described in {processMultiProof}.
-     *
-     * _Available since v4.7._
-     */
-    function multiProofVerify(
-        bytes32[] memory proof,
-        bool[] memory proofFlags,
-        bytes32 root,
-        bytes32[] memory leaves
-    ) internal pure returns (bool) {
-        return processMultiProof(proof, proofFlags, leaves) == root;
-    }
-
-    /**
-     * @dev Calldata version of {multiProofVerify}
-     *
-     * _Available since v4.7._
-     */
-    function multiProofVerifyCalldata(
-        bytes32[] calldata proof,
-        bool[] calldata proofFlags,
-        bytes32 root,
-        bytes32[] memory leaves
-    ) internal pure returns (bool) {
-        return processMultiProofCalldata(proof, proofFlags, leaves) == root;
-    }
-
-    /**
-     * @dev Returns the root of a tree reconstructed from `leaves` and the sibling nodes in `proof`,
-     * consuming from one or the other at each step according to the instructions given by
-     * `proofFlags`.
-     *
-     * _Available since v4.7._
-     */
-    function processMultiProof(
-        bytes32[] memory proof,
-        bool[] memory proofFlags,
-        bytes32[] memory leaves
-    ) internal pure returns (bytes32 merkleRoot) {
-        // This function rebuild the root hash by traversing the tree up from the leaves. The root is rebuilt by
-        // consuming and producing values on a queue. The queue starts with the `leaves` array, then goes onto the
-        // `hashes` array. At the end of the process, the last hash in the `hashes` array should contain the root of
-        // the merkle tree.
-        uint256 leavesLen = leaves.length;
-        uint256 totalHashes = proofFlags.length;
-
-        // Check proof validity.
-        require(leavesLen + proof.length - 1 == totalHashes, "MerkleProof: invalid multiproof");
-
-        // The xxxPos values are "pointers" to the next value to consume in each array. All accesses are done using
-        // `xxx[xxxPos++]`, which return the current value and increment the pointer, thus mimicking a queue's "pop".
-        bytes32[] memory hashes = new bytes32[](totalHashes);
-        uint256 leafPos = 0;
-        uint256 hashPos = 0;
-        uint256 proofPos = 0;
-        // At each step, we compute the next hash using two values:
-        // - a value from the "main queue". If not all leaves have been consumed, we get the next leaf, otherwise we
-        //   get the next hash.
-        // - depending on the flag, either another value for the "main queue" (merging branches) or an element from the
-        //   `proof` array.
-        for (uint256 i = 0; i < totalHashes; i++) {
-            bytes32 a = leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++];
-            bytes32 b = proofFlags[i] ? leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++] : proof[proofPos++];
-            hashes[i] = _hashPair(a, b);
-        }
-
-        if (totalHashes > 0) {
-            return hashes[totalHashes - 1];
-        } else if (leavesLen > 0) {
-            return leaves[0];
-        } else {
-            return proof[0];
-        }
-    }
-
-    /**
-     * @dev Calldata version of {processMultiProof}
-     *
-     * _Available since v4.7._
-     */
-    function processMultiProofCalldata(
-        bytes32[] calldata proof,
-        bool[] calldata proofFlags,
-        bytes32[] memory leaves
-    ) internal pure returns (bytes32 merkleRoot) {
-        // This function rebuild the root hash by traversing the tree up from the leaves. The root is rebuilt by
-        // consuming and producing values on a queue. The queue starts with the `leaves` array, then goes onto the
-        // `hashes` array. At the end of the process, the last hash in the `hashes` array should contain the root of
-        // the merkle tree.
-        uint256 leavesLen = leaves.length;
-        uint256 totalHashes = proofFlags.length;
-
-        // Check proof validity.
-        require(leavesLen + proof.length - 1 == totalHashes, "MerkleProof: invalid multiproof");
-
-        // The xxxPos values are "pointers" to the next value to consume in each array. All accesses are done using
-        // `xxx[xxxPos++]`, which return the current value and increment the pointer, thus mimicking a queue's "pop".
-        bytes32[] memory hashes = new bytes32[](totalHashes);
-        uint256 leafPos = 0;
-        uint256 hashPos = 0;
-        uint256 proofPos = 0;
-        // At each step, we compute the next hash using two values:
-        // - a value from the "main queue". If not all leaves have been consumed, we get the next leaf, otherwise we
-        //   get the next hash.
-        // - depending on the flag, either another value for the "main queue" (merging branches) or an element from the
-        //   `proof` array.
-        for (uint256 i = 0; i < totalHashes; i++) {
-            bytes32 a = leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++];
-            bytes32 b = proofFlags[i] ? leafPos < leavesLen ? leaves[leafPos++] : hashes[hashPos++] : proof[proofPos++];
-            hashes[i] = _hashPair(a, b);
-        }
-
-        if (totalHashes > 0) {
-            return hashes[totalHashes - 1];
-        } else if (leavesLen > 0) {
-            return leaves[0];
-        } else {
-            return proof[0];
-        }
     }
 
     function _hashPair(bytes32 a, bytes32 b) private pure returns (bytes32) {
@@ -286,7 +94,6 @@ library MerkleProof {
 }
 
 
-
 /**
  * @title NFTX ENS Eligibility
  * @author Twade
@@ -297,7 +104,7 @@ library MerkleProof {
 contract NFTXMerkleEligibility is NFTXEligibility {
 
     /// @notice Emitted when our NFTX Eligibility is deployed
-    event NFTXEligibilityInit(bytes32 merkleRoot);
+    event NFTXEligibilityInit(string name, address asset, bytes32 merkleRoot);
 
     /// @notice Emitted when a project validity check is started
     event PrecursoryCheckStarted(uint tokenId, bytes32 requestId);
@@ -312,6 +119,12 @@ contract NFTXMerkleEligibility is NFTXEligibility {
     /// @notice Merkle proof to validate all eligible domains against
     bytes32 public merkleRoot;
 
+    /// @notice The name of our eligilibity module
+    string private name;
+
+    /// @notice The asset address of the handled token
+    address private asset;
+
 
     /**
      * @notice The name of our Eligibility Module.
@@ -320,7 +133,7 @@ contract NFTXMerkleEligibility is NFTXEligibility {
      */
 
     function name() public pure override virtual returns (string memory) {    
-        return "ENSValidation";
+        return name;
     }
 
 
@@ -336,13 +149,13 @@ contract NFTXMerkleEligibility is NFTXEligibility {
 
 
     /**
-     * @notice ..
+     * @notice The address of our token asset contract.
      *
      * @return address 
      */
 
    function targetAsset() public pure override virtual returns (address) {
-        return 0x57f1887a8BF19b14fC0dF6Fd9B2acc9Af147eA85;
+        return asset;
     }
 
 
@@ -350,36 +163,42 @@ contract NFTXMerkleEligibility is NFTXEligibility {
      * @notice Allow our eligibility module to be initialised with optional
      * config data.
      * 
-     * @dev As there is no logic required in the init, we just directly
-     * call `__NFTXEligibility_init`.
+     * @param configData Encoded config data
      */
 
     function __NFTXEligibility_init_bytes(bytes memory configData) public override virtual initializer {
-        (bytes32 _merkleRoot) = abi.decode(configData, (bytes32));
-        __NFTXEligibility_init(_merkleRoot);
+        (string _name, address _asset, bytes32 _merkleRoot) = abi.decode(configData, (string, address, bytes32));
+        __NFTXEligibility_init(_name, _asset, _merkleRoot);
     }
 
 
     /**
      * @notice Parameters here should mirror the config struct.
+     * 
+     * @param _name The name of our eligibility module
+     * @param _asset The address of our token contract
+     * @param _merkleRoot The root of our merkle tree
      */
 
-    function __NFTXEligibility_init(bytes32 _merkleRoot) public initializer {
+    function __NFTXEligibility_init(string _name, address _asset, bytes32 _merkleRoot) public initializer {
+        name = _name
+        asset = _asset;
         merkleRoot = _merkleRoot;
-        emit NFTXEligibilityInit(_merkleRoot);
+
+        emit NFTXEligibilityInit(_name, _asset _merkleRoot);
     }
 
 
     /**
-     * @notice Checks if a supplied token is eligible, which is defined by being
-     * part of a "curated" ArtBlocks project.
+     * @notice Checks if a supplied token is eligible, which is defined by our merkle
+     * tree root assigned at initialisation.
+     * 
+     * @dev This check requires the token to have already been passed to `processToken`.
      *
      * @return bool If the tokenId is eligible
      */
 
     function _checkIfEligible(uint tokenId) internal view override virtual returns (bool) {
-        // If we have a `projectEligibility` value already stored, then we can return
-        // an eligibility boolean based on the integer value assigned.
         return validTokenHashes[keccak256(bytes(Strings.toString(tokenId)))];
     }
 
@@ -387,8 +206,8 @@ contract NFTXMerkleEligibility is NFTXEligibility {
      * @notice Checks if the token requires a precursory validation before it can have
      * it's eligibility determined.
      * 
-     * @dev If this returns `true`, `runPrecursoryValidation` should subsequently be run
-     * before checking the eligibility of the token.
+     * @dev If this returns `true`, `processToken` should subsequently be run before
+     * checking the eligibility of the token.
      * 
      * @param tokenId The ENS domain token ID
      *
@@ -396,22 +215,16 @@ contract NFTXMerkleEligibility is NFTXEligibility {
      */
 
     function requiresProcessing(uint tokenId) public view returns (bool) {
-        // If our value is not yet mapped, then we require a precursory validation
-        // to be performed.
         return !_processedTokenHashes[keccak256(bytes(Strings.toString(tokenId)))];
     }
 
 
     /**
-     * @notice This will run a precursory check by checking the ArtBlocks token metadata
-     * via a direct API call. This uses ChainLink to ensure the data is stored on chain
-     * and will be confirmed in a subsequent callback.
-     * 
-     * When the precursory check is completed, a subsequent event will be fired that
-     * will allow a frontend client to confirm it has been completed.
-     * 
+     * @notice This will run a precursory check by encoding the token ID, creating the
+     * token hash, and then checking this against our merkle tree.
+     *
      * @param tokenId The ENS token ID being validated
-     * @param merkleProof Hash of the merkle proof
+     * @param merkleProof Merkle proof to validate against the tokenId
      *
      * @return bool If the token is valid
      */
