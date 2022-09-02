6ace6d67714b5932e15c599d3b832fb1e15d69b6
Tom Wade <twade7@gmail.com>
2022-09-02 15:02:48 +0100

    Add test and deploy script

--- contracts/solidity/NFTXInventoryStaking.sol
+++ contracts/solidity/NFTXInventoryStaking.sol
@@ -86,7 +86,6 @@ contract NFTXInventoryStaking is PausableUpgradeable, UpgradeableBeacon, INFTXIn
 
     function timelockMintFor(uint256 vaultId, uint256 amount, address to, uint256 timelockLength) external virtual override returns (uint256) {
         onlyOwnerIfPaused(10);
-        require(msg.sender == nftxVaultFactory.zapContract(), "Not a zap");
         require(nftxVaultFactory.excludedFromFees(msg.sender), "Not fee excluded");
 
         (, , uint256 xTokensMinted) = _timelockMintFor(vaultId, to, amount, timelockLength);
