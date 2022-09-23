edd0dcfc83971f2c204ff13c182b361563edd28b
Tom Wade <twade7@gmail.com>
2022-09-23 15:47:53 +0100

    Make swap target immutable on deployment

--- scripts/deploy-yield-staking-zap.js
+++ scripts/deploy-yield-staking-zap.js
@@ -19,6 +19,7 @@ async function main() {
     "0x33b381E2e0c4adC1dbd388888e9A29079e5b6702",  // LP Staking
     "0x1b02dA8Cb0d097eB8D57A175b88c7D8b47997506",  // SushiSwapRouter
     "0xb4fbf271143f4fbf7b91a5ded31805e42b2208d6",  // WETH
+    "0xf91bb752490473b8342a3e964e855b9f9a2a668e",  // 0x Swap Target
   );
   // Mainnet
   // const zap = await YieldStakingZap.deploy(
@@ -27,6 +28,7 @@ async function main() {
   //    "0x688c3E4658B5367da06fd629E41879beaB538E37",  // LP Staking
   //    "0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F",  // SushiSwapRouter
   //    "0xC02aaA39b223FE8D0A0e5C4F27eAD9083C756Cc2",  // WETH
+  //    "0xdef1c0ded9bec7f1a1670819833240f027b25eff",  // 0x Swap Target
   // );
   await zap.deployed();
   console.log("Yield Staking Zap:", zap.address);
